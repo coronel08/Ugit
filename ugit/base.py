fab5627ca87faaa86bda81f741f636cd6128cca2
Fidel <coronel08@aol.com>
2020-09-28 11:49:59 -0700

    made the base write files instead of just print and discard

--- ugit/base.py
+++ ugit/base.py
@@ -5,10 +5,17 @@ def write_tree(directory='.'):
     with os.scandir(directory) as it:
         for entry in it:
             full = f'{directory}/{entry.name}'
-            if entry.is_file (follow_symlinks=False):
-                #TODO write the file object store here
-                print(full)
+            if is_ignored(full):
+                continue
+
+            if entry.is_file(follow_symlinks=False):
+                with open(full, 'rb') as f:
+                    print(data.hash_objects(f.read()), full)
             elif entry.is_dir(follow_symlinks=False):
                 write_tree(full)
 
     #TODO actually create the tree object
+
+
+def is_ignored(path):
+    return '.ugit' in path.split('/')
