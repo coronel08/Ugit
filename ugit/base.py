c6564865786ff8e5fdc33121fb10528cf3494f75
Fidel <coronel08@aol.com>
2020-09-28 14:32:34 -0700

    Create Commit sections in base and cli

--- ugit/base.py
+++ ugit/base.py
@@ -74,5 +74,12 @@ def read_tree(tree_oid):
             f.write(data.get_object(oid))
 
 
+def commit(message):
+    commit = f'tree {write_tree()}\n'
+    commit += '\n'
+    commit += f'{message}\n'
+
+    return data.hash_objects(commit.encode(), 'commit')
+
 def is_ignored(path):
     return '.ugit' in path.split('/')
