9452b6c84a2110e0c8d57de85afa4a4845fa4402
Fidel <coronel08@aol.com>
2020-10-05 10:38:01 -0700

    Edit write tree to use index instead of working directory

--- ugit/base.py
+++ ugit/base.py
@@ -14,25 +14,35 @@ def init():
 
 
 def write_tree(directory='.'):
-    entries = []
-    with os.scandir(directory) as it:
-        for entry in it:
-            full = f'{directory}/{entry.name}'
-            if is_ignored(full):
-                continue
+    # Index is flat, we need it as a tree of dicts
+    index_as_tree = {}
+    with data.get_index() as index:
+        for path, oid, in index.items():
+            path = path.split('/')
+            dirpath, filename = path[:-1], path[-1]
 
-            if entry.is_file(follow_symlinks=False):
-                type_ = 'blob'
-                with open(full, 'rb') as f:
-                    oid = data.hash_objects(f.read())
-            elif entry.is_dir(follow_symlinks=False):
+            current = index_as_tree
+            # Find the dict for the dictionary of this file
+            for dirname in dirpath:
+                current = current.setdefault(dirname, {})
+            current[filename] = oid
+
+    def write_tree_recursive(tree_dict):
+        entries = []
+        for name, value in tree_dict.items():
+            if type(value) is dict:
                 type_ = 'tree'
-                oid = write_tree(full)
-            entries.append((entry.name, oid, type_))
+                oid = write_tree_recursive(value)
+            else:
+                type = 'blob'
+                oid = value
+            entries.append((name, oid, type_))
 
-    tree = ''.join(f'{type_} {oid} {name}\n'
-                   for name, oid, type_ in sorted(entries))
-    return data.hash_objects(tree.encode(), 'tree')
+        tree = ''.join(f'{type_} {oid} {name}\n'
+                       for name, oid, type_
+                       in sorted(entries))
+        return data.hash_objects(tree.encode(), 'tree')
+    return write_tree_recursive(index_as_tree)
 
 
 def _iter_tree_entries(oid):
